4591de565f5b1f8cc4e409b325e197e16e31f696
JSFernandes <joseph.automatus@gmail.com>
2023-07-04 23:37:45 -0300

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -7,54 +7,62 @@ while true; do
     echo "2. Personalizar cor hexadecimal"
     echo "3. Realizar build do frontend"
     echo "4. Sair"
-
+    
     read -p "Opção: " opcao
-
+    
     case $opcao in
         1)
             read -p "Qual o nome que gostaria para o app Multi100? " empresa
-
-            sudo -E -u deploy bash -c '
-            cd /home/deploy/mod_100
+            
+            sleep 2
+            
+            sudo su - deploy <<EOF
+              cd /home/deploy/mod_100
             unzip -o mod_login.zip -d .
             rm mod_login.zip
             cp -Rf /home/deploy/mod_100/assets/* /home/multi100/frontend/src/assets
             cp -f /home/deploy/mod_100/index.js /home/multi100/frontend/src/pages/login
             cp -Rf /home/deploy/mod_100/public/* /home/multi100/frontend/public
-            '
 
             sed -i "s/REACT_APP_NAME_SYSTEM=\"Multi100\"/REACT_APP_NAME_SYSTEM=\"$empresa\"/" /home/deploy/multi100/frontend/.env
 
-            cd /home/multi100/frontend
+            cd /home/deploy/multi100/frontend
             npm install react-lottie --force
-
+EOF
+            
+            sleep 2
+            
             read -p "Pressione qualquer tecla para continuar..."
-            ;;
-
+        ;;
+        
         2)
             read -p "Para qual cor hexadecimal quer trocar (PADRÃO #34BCFF)? " corhex
-
+            
             find /home/deploy/multi100/frontend/ -type f -exec sed -i "s/#34BCFF/$corhex/g" {} \;
-
+            
             read -p "Pressione qualquer tecla para continuar..."
-            ;;
-
+        ;;
+        
         3)
-            sudo -E -u deploy bash -c '
+            sleep 2
+            
+            sudo su - deploy <<EOF
             cd /home/multi100/frontend
             npm run build
             pm2 restart all
-            '
-
+EOF
+            
+            sleep 2
+            
             read -p "Pressione qualquer tecla para continuar..."
-            ;;
-
+        ;;
+        
         4)
             exit
-            ;;
-
+        ;;
+        
         *)
             echo "Opção inválida. Pressione qualquer tecla para continuar..."
-            ;;
+        ;;
     esac
 done
